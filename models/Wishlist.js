abb837fbcead9050a8e6dae3d20c31df00db7a86
Ammar Ahmed <ammarahmed1263@gmail.com>
2026-07-07 00:03:41 +0300

    fix dublicate id

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -28,7 +28,6 @@ const wishlistSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-wishlistSchema.index({ user: 1 });
 
 const Wishlist = mongoose.model('Wishlist', wishlistSchema);
 export default Wishlist;
